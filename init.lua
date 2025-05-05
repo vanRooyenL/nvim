1d7251c1a2830f5a09053409bbb949386b6ec31c
vanRooyenL <vanrooyenl@outlook.com>
2025-05-05 16:33:22 +0200

    Added Basic Tree Sitter Config

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+require 'core.options'
+require 'core.keymaps'
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
@@ -17,12 +19,34 @@ require('lazy').setup({
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
     -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-  },
-  lazy = false, -- neo-tree will lazily load itself
-  ---@module "neo-tree"
-  ---@type neotree.Config?
-  opts = {
-    -- fill any relevant options here
-  },
+	}
+},
+{
+'shaunsingh/nord.nvim',
+lazy = false,
+priority = 1000,
+config = function()
+	-- Example config in lua
+	vim.g.nord_contrast = true
+	vim.g.nord_borders = false
+	vim.g.nord_disable_background = false
+	vim.g.nord_italic = false
+	vim.g.nord_uniform_diff_background = true
+	vim.g.nord_bold = false
+
+	-- Load the colorscheme
+	require('nord').set()
+end
+},
+{
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  config = function()
+    require('nvim-treesitter.configs').setup {
+      ensure_installed = { "lua", "vim", "bash", "markdown", "json" }, -- add others as needed
+      highlight = { enable = true },
+      indent = { enable = true },
+    }
+  end,
 }
 })
