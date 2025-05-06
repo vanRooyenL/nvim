3163ae0c64c2efdfc5ff973a02c2d93ccc1871ec
vanRooyenL <vanrooyenl@outlook.com>
2025-05-06 13:58:39 +0200

    + lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,8 +5,6 @@ return { -- LSP Configuration & Plugins
     'williamboman/mason.nvim',
     'williamboman/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
-    'hrsh7th/cmp-nvim-lsp',
-    
     -- Useful status updates for LSP.
     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
     {
