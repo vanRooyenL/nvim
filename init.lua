c81e2db67ef65f482303352a0e554afa271e1734
vanRooyenL <vanrooyenl@outlook.com>
2025-05-06 14:34:31 +0200

    + Autoformatter + Autocompletion

--- init.lua
+++ init.lua
@@ -1,28 +1,30 @@
-require 'core.options'
-require 'core.keymaps'
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+require("core.options")
+require("core.keymaps")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
-end 
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
+end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
+require("lazy").setup({
 
--- Select Theme
-require 'themes.onedark',
--- require 'themes.nord',
--- Plugins are declared as required here as found in the /core/plugins
-require 'plugins.neotree',    
--- require 'plugins.colortheme',
-require 'plugins.treesitter',
-require 'plugins.bufferline',
-require 'plugins.lualine',
-require 'plugins.treesitter',
-require 'plugins.telescope',
-require 'plugins.lazygit',
-require 'plugins.lsp'
+	-- Select Theme
+	require("themes.onedark"),
+	-- require 'themes.nord',
+	-- Plugins are declared as required here as found in the /core/plugins
+	require("plugins.neotree"),
+	-- require 'plugins.colortheme',
+	require("plugins.treesitter"),
+	require("plugins.bufferline"),
+	require("plugins.lualine"),
+	require("plugins.treesitter"),
+	require("plugins.telescope"),
+	require("plugins.lazygit"),
+	require("plugins.lsp"),
+	require("plugins.autocompletion"),
+	require("plugins.autoformattiong"),
 })
