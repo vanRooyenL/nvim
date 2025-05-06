ef5308b6f3fb1a534172b32de96bdab26c5c346e
vanRooyenL <vanrooyenl@outlook.com>
2025-05-06 14:46:12 +0200

    + alpha + gitsigns

--- init.lua
+++ init.lua
@@ -2,29 +2,31 @@ require("core.options")
 require("core.keymaps")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		error("Error cloning lazy.nvim:\n" .. out)
-	end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        error("Error cloning lazy.nvim:\n" .. out)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 
-	-- Select Theme
-	require("themes.onedark"),
-	-- require 'themes.nord',
-	-- Plugins are declared as required here as found in the /core/plugins
-	require("plugins.neotree"),
-	-- require 'plugins.colortheme',
-	require("plugins.treesitter"),
-	require("plugins.bufferline"),
-	require("plugins.lualine"),
-	require("plugins.treesitter"),
-	require("plugins.telescope"),
-	require("plugins.lazygit"),
-	require("plugins.lsp"),
-	require("plugins.autocompletion"),
-	require("plugins.autoformattiong"),
+    -- Select Theme
+    require("themes.onedark"),
+    -- require 'themes.nord',
+    -- Plugins are declared as required here as found in the /core/plugins
+    require("plugins.neotree"),
+    -- require 'plugins.colortheme',
+    require("plugins.treesitter"),
+    require("plugins.bufferline"),
+    require("plugins.lualine"),
+    require("plugins.treesitter"),
+    require("plugins.telescope"),
+    require("plugins.lazygit"),
+    require("plugins.lsp"),
+    require("plugins.autocompletion"),
+    require("plugins.autoformattiong"),
+    require("plugins.gitsigns"),
+    require("plugins.alpha"),
 })
